f53fda10288bb178290df015b0fe0ef50a3bc337
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-07-01 21:20:32 +0300

    Packages installed

---
+++ PetConnect.DAL/Data/_GenericRepository/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConnect.DAL.Data.GenericRepository
+{
+    internal class GenericRepository<T> : IGenericRepository<T>
+    {
+        public GenericRepository()
+        {
+            
+        }
+        public void Add(T entity)
+        {
+         
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PetConnect.DAL/Data/_GenericRepository/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConnect.DAL.Data.GenericRepository
+{
+    internal interface IGenericRepository<T>   // T > Entity
+    {
+
+        public IEnumerable<T> GetAll(); // Get All T
+
+        public T? GetByID(int id); // Get Element By Id
+
+        public void Add(T entity); // Add entity record to DbSet
+
+        public void Update(T entity); // update entity data from existing DbSet
+
+        public void Delete(T entity); // delete entity data from existing DbSet
+
+    }
+}
