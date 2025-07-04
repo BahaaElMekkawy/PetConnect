56c2f3cf49e14ceffe78f0a038da450e32d83b0e
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-07-04 17:57:02 +0300

    Dev Branch

---
+++ PetConnect.BLL/Services/ServicesRegisteration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PetConnect.DAL.Data.Repositories.Classes;
+using PetConnect.DAL.Data.Repositories.Interfaces;
+using PetConnect.DAL.UnitofWork;
+
+namespace PetConnect.DAL.Services
+{
+    public static class ServicesCollectionExtensions
+    {
+        public static IServiceCollection AddDalRepositories(this IServiceCollection services)
+        {
+            // Services 
+
+
+            return services;
+        }
+    }
+}
