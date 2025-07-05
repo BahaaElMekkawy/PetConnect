c13dc441723ae2e6a7eb522fc9f4c99cdb8664e0
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-07-05 06:01:12 +0300

    Pet Bread Done Without Handling Navigation

---
+++ PetConnect.BLL/Services/Classes/PetService.cs
@@ -0,0 +1,13 @@
+﻿using PetConnect.BLL.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConnect.BLL.Services.Classes
+{
+    public class PetService : IPetService
+    {
+    }
+}
---
+++ PetConnect.BLL/Services/DTO/PetBreadDto/UPetBreadDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConnect.BLL.Services.DTO.PetBreadDto
+{
+    public class UPetBreadDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "The Id is Required")]
+        public int CategoryId { get; set; }
+    }
+}
